7a40f1884109ac9dcfb799ded374209b1cd40ab8
mnogodumal <raskol@duck.com>
2024-12-22 14:29:03 +0300

    module8-task2

--- src/pages/OfferNotLogged.tsx
+++ src/pages/OfferNotLogged.tsx
@@ -189,7 +189,7 @@ function OfferNotLogged() {
                       <p className="reviews__text">
                         A quiet cozy and picturesque that hides behind a a river
                         by the unique lightness of Amsterdadm. The building is
-                        green and from 18th century.fdfrdfd
+                        green and from 18th century.fdfrdfdd
                       </p>
                       <time className="reviews__time" dateTime="2019-04-24">
                         April 2019
